7ee751323a42688bb944bc45df2a2d1bba58aa4d
Sahil Satralkar <62758655+sahilsatralkar@users.noreply.github.com>
2026-03-10 20:08:30 +0530

    feat: integrate interaction state into modifier

--- Sources/SwiftChartInteractions/ChartInteractionsModifier.swift
+++ Sources/SwiftChartInteractions/ChartInteractionsModifier.swift
@@ -3,6 +3,7 @@ import SwiftUI
 public struct ChartInteractionsModifier: ViewModifier {
     
     let configuration: ChartInteractionsConfiguration
+    @StateObject private var state = ChartInteractionState()
     
     public func body(content: Content) -> some View {
         content
