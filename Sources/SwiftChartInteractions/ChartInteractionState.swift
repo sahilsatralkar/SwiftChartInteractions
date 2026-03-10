0d72c497dac4cbdb345037343c1d462d7305f6e5
Sahil Satralkar <62758655+sahilsatralkar@users.noreply.github.com>
2026-03-10 20:15:09 +0530

    feat: add range selection state

--- Sources/SwiftChartInteractions/ChartInteractionState.swift
+++ Sources/SwiftChartInteractions/ChartInteractionState.swift
@@ -4,5 +4,7 @@ import Combine
 final class ChartInteractionState: ObservableObject {
     
     @Published var selectedLocation: CGPoint?
+    @Published var dragStart: CGPoint?
+    @Published var dragEnd: CGPoint?
     
 }
