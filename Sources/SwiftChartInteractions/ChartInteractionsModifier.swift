467dacddffc2f6459e0fe11c17bb271d07c1f1f4
Sahil Satralkar <62758655+sahilsatralkar@users.noreply.github.com>
2026-03-10 20:06:50 +0530

    feat: add base view modifier for chart interactions

---
+++ Sources/SwiftChartInteractions/ChartInteractionsModifier.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+public struct ChartInteractionsModifier: ViewModifier {
+    
+    let configuration: ChartInteractionsConfiguration
+    
+    public func body(content: Content) -> some View {
+        content
+    }
+}
